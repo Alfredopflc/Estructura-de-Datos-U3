e61fb59680853cb7663954b0a660d3c7abf265c2
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-10-12 12:49:12 -0700

    Add files via upload

---
+++ AC.3-2-VenegasMedinaJoseAlfredo/AC.3-2-VenegasMedinaJoseAlfredo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC._3_2_VenegasMedinaJoseAlfredo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Count = 10;
+
+            Stack Pila = new Stack();
+
+            for (int i = 0; i < 10; i++)
+                Pila.Push(i+1);
+
+            foreach(var i in Pila)
+            {
+                Console.WriteLine("Valor: {0}    Tipo:{1}   Posicion: {2}", i, i.GetType(), Count);
+                Count--;
+            }
+        }
+    }
+}
