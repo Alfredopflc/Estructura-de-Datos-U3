84e4d112b1baa74f9e08e18ad47eb7797aeea627
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-10-15 12:54:59 -0700

    Add files via upload

---
+++ QueueClass/QueueClass/Cola.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueClass
+{
+    public class Cola
+    {
+        private ArrayList pcola;
+        public Cola()
+        {
+            pcola = new ArrayList();
+        }
+
+        public void EnQueue(object item)
+        { 
+            //Añade objeto de entrada al ArrayList
+            //El objeto puede ser cualquier tipo de dato
+            pcola.Add(item);
+        }
+
+        public void DeQueue()
+        {
+            //Elimina la posicion 0 del ArrayList
+            pcola.RemoveAt(0);
+        }
+
+        public object Peek()
+        {
+            //Visualiza el elemento 0
+            //pero en la mente de Jesus es el 1 :v
+            return pcola[0];
+        }
+
+        public void ClearQueue()
+        {
+            //Limpia la Cola
+            pcola.Clear();
+        }
+
+        public int Count ()
+        {
+            //Regresa la cantidad de elementos de la cola 
+            return pcola.Count;
+        }
+
+    }
+}
---
+++ QueueClass/QueueClass/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cola MyCola = new Cola();
+
+            for (int i = 0; i < 5; i++)
+            {
+                MyCola.EnQueue(i);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+              // =  Console.WriteLine(MyCola.Peek());
+                MyCola.DeQueue();
+            }
+
+            Console.ReadKey();
+
+            //Inicializacion de Colas con datos 
+            Queue queue = new Queue(100);
+
+            //La capacidad de la Cola y Factor de crecimiento (constante de crecimiento)
+            Queue myQueue = new Queue(100, 3);
+
+            //Cola con tipo de datos especifico
+            Queue<int> numeros = new Queue<int>();
+
+            //Tradicional
+            Queue myc = new Queue();
+
+        }
+    }
+}
