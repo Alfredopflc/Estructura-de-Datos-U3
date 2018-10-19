3748f80ed0cf469284bf49c95209143399d18a09
JOSE VENEGAS <42922208+Alfredopflc@users.noreply.github.com>
2018-10-19 16:57:30 -0700

    Add files via upload

---
+++ E3.3-VenegasMedinaJoseAlfredo/E3.3-VenegasMedinaJoseAlfredo/BlackJack.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3._3.BlackJack
+{
+    class BlackJack //Inicia Clase BlackJack
+    {
+        Stack Simbolos = new Stack(); //Pila para los simbolos de las cartas
+        Stack Numeros = new Stack(); //Pila para los numeros
+        Random random = new Random(); //Metodo para revolver baraja
+
+        char[] valores = new char[52];
+        int[] valores2 = new int[52];
+       
+        int suma = 0, num, cantjuegos, opc, partgan, partper;
+        string mano = "";
+        bool Salir = false;
+
+        public BlackJack(int CantJuegos, int Ganados, int Perdidos) //Constructor por defecto
+        {
+            cantjuegos = CantJuegos;
+            partgan = Ganados;
+            partper = Perdidos;
+        }
+
+        public int Carga() //Metodo para llenar los valores de las cartas
+        {
+            for (int i = 1, j = 0; i < 5; i++) //For para llenar los arreglos con los valores de cartas
+            {
+                for (int k = 1; k <= 13; k++)
+                {
+                    if (k >= 10) //Si la carta es mayor que 10 (J,Q,K) Valdra 10
+                    {
+                        valores2[j] = 10;
+                    }
+                    else //Si no, el valor de k del 1 al 10
+                    {
+                        valores2[j] = k;
+                    }
+
+                    j++;
+                }
+            }
+
+            for (int i = 0; i < 52; i++) //For para llenar el arreglo con los simbolos
+            {
+                if (i < 13) //13 cartas de cada simbolo
+                {
+                    valores[i] = (char)3;
+                }
+                else if (i < 26)
+                {
+                    valores[i] = (char)4;
+                }
+                else if (i <= 38)
+                {
+                    valores[i] = (char)5;
+                }
+                else
+                {
+                    valores[i] = (char)6;
+                }
+            }
+
+            Random(valores); //Una vez llenado los arreglos, los pondremos en el metodo Random, para "barajear" los elementos
+            Random(valores2);
+
+            for (int Contador = 0; Contador < 52; Contador++) //Se llena la Pila Simbolos y Numeros con los elementos de los vectores
+            {
+                Simbolos.Push(valores[Contador]);
+                Numeros.Push(valores2[Contador]);
+            }
+
+            SacarCarta(); //Se deben sacar 2 cartas al principio
+            SacarCarta();
+
+            return suma; //Regresamos la suma 
+        }
+
+        public void Random<T>(T[] array) //Metodo para "barajear"  los valores
+        {
+            int n = array.Length; //El tamano del arreglo definira cuantas veces se repetira el for
+
+            for (int i = 0; i < n; i++) //Se revuelven los valores
+            {
+                int r = i + random.Next(n - i);
+                T t = array[r];
+                array[r] = array[i];
+                array[i] = t;
+            }
+        }
+
+        public int SacarCarta() //Metodo para sacar la carta y no se vuelva a repetir
+        {
+            num = Convert.ToInt16(Numeros.Peek());
+
+            if (num == 1) //Si sale 1, se considerara como un Az
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("~~ Partida Numero: {0} ~~", cantjuegos); //Marca cuantas partidas se han jugado 
+
+                    Console.Write("\nTe salio un az, desea usarlo como: \n1 = Uzarlo como 1 \n2 = Usarlo como 11 \nR = "); 
+                    //Al salir un az, este puede valer 1 u 11, el usuario decidira como tomarlo
+                    opc = int.Parse(Console.ReadLine());
+
+                    if (opc == 1) //Si elige la opcion 1, el valor valdra 1 y se sumara al valor suma
+                    {
+                        num = 1;
+                        Salir = true;
+                    }
+
+                    else if (opc == 2) //Si elige la opcion 2, el valor valdra 11 y se sumara al valor suma
+                    {
+                        num = 11;
+                        Salir = true;
+                    }
+
+                    else //Si no, marcara un error
+                    {
+                        Console.WriteLine("ERROR");
+                        Console.ReadKey();
+                        Salir = false;
+                    }
+
+                } while (Salir == false); //Si el numero no es 1 (Un az) o termina el proceso de eleccion entonces....
+            }
+            
+            Console.Clear();
+            Console.WriteLine("~~ Partida Numero: {0} ~~", cantjuegos); //Mostrara el numero de partidas jugadas
+
+            Console.WriteLine("Juegos ganados: {0}  \tJuegos perdidos: {1}", partgan, partper); 
+            //Tambien te mostrara cuantas partidas has ganado o perdido, si es el primer juego, marcara 0 en ambos
+            suma = suma + num; //Hara la suma de cada carta
+
+            Console.WriteLine("\nSuma total: {0}", suma); //Te mostrara la suma de tus cartas
+            mano = mano + Simbolos.Pop() + " " + num + "\t"; //Te mostrara los valores de las cartas y sus simbolos
+            Numeros.Pop(); //Saca el valor de la pila
+
+            Console.WriteLine(mano); //Imprime toda tu mano actual
+
+            return suma; //Regresa la suma total de tu mano
+        }
+    }
+}
---
+++ E3.3-VenegasMedinaJoseAlfredo/E3.3-VenegasMedinaJoseAlfredo/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3._3.BlackJack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantjuegos = 0, opc = 0, partgan = 0, partper = 0; //Inicializar variables
+            bool Salir = false;
+
+            do
+            {
+                bool salida1 = false, salida2 = false;
+                int suma = 0, Cartas = 2;
+                cantjuegos++;
+
+                BlackJack BJ = new BlackJack(cantjuegos, partgan, partper); //Se crea un objeto de la clase BlackJack
+                suma = BJ.Carga();
+
+                do
+                {
+                    if (suma < 21) //Si suma de tus cartas es menor que 21, entonces se ejecutara lo siguiente
+                    {
+                        do
+                        {
+                            if (Cartas < 5) //Si tienes menos de 5 cartas, te pedira si quieres otra para continuar sumando
+                            {
+                                Console.Write("\nDeseas otra carta? \n1 = Si \n2 = No \nR = ");
+                                opc = int.Parse(Console.ReadLine());
+
+                                if (opc == 1) //Si deseas sacar la carta, la suma sera regresada por el metodo SacarCartas de la clase BlackJack
+                                {
+                                    suma = BJ.SacarCarta();
+                                    Cartas++;
+                                    salida2 = true;
+                                }
+
+                                else if (opc == 2) //Si no, el programa te dira que perdiste por no llegar al 21
+                                {
+                                    Console.WriteLine("HAZ PERDIDO :(");
+                                    salida1 = true; //Saldras del programa y te preguntara si quieres volver empezar
+                                    salida2 = true;
+                                }
+
+                                else //Marcara un error si no elegiste 1 o 2
+                                {
+                                    Console.WriteLine("ERROR");
+                                    Console.ReadKey();
+                                    Console.Clear();
+                                }                     
+                            }
+
+                            else //En el caso de ya tener 5 cartas
+                            {
+                                salida2 = true;
+
+                                if (suma < 21) //El programa al tener una suma menor de 21, dira que perdiste
+                                {
+                                    Console.WriteLine("\nHAZ PERDIDO :(");
+                                    partper++;
+                                    salida1 = true;
+                                }
+                                else if (suma == 21) //Si es igual a 21 ganaste
+                                {
+                                    Console.WriteLine("\nHAZ GANADO :)");
+                                    partgan++;
+                                    salida1 = true;
+                                }
+                                else //Si te pasas del 21, habras perdido
+                                {
+                                    Console.WriteLine("\nHAZ PERDIDO :(");
+                                    partper++;
+                                    salida1 = true;
+                                }
+
+                            }
+
+                        } while (salida2 == false); //Regresara si aun no has tomado 5 cartas y tu suma no ha pasado de 21
+                    }
+
+                    else if (suma == 21) //Si sale 21 antes de las 5 cartas, habras ganado
+                    {
+                        Console.WriteLine("\nHAZ GANADO :)");
+                        partgan++;
+                        salida1 = true;
+                    }
+
+                    else //De lo contrario, habras perdido
+                    {
+                        Console.WriteLine("\nHAZ PERDIDO :(");
+                        partper++;
+                        salida1 = true;
+                    }
+
+                } while (salida1 == false); //Sale del ciclo de eleccion de cartas
+
+                Console.WriteLine("Presiona cualquier tecla para continuar.");
+                Console.ReadKey();
+                salida1 = false;
+
+                do
+                {
+                    Console.Clear();
+                    Console.Write("Deseas jugar de nuevo? \n1 = Si \n2 = No \nR = "); //Te preguntara si quieres volver a jugar
+                    opc = int.Parse(Console.ReadLine());
+
+                    if (opc == 1) //Si elijes 1, volveras a empezar
+                    {
+                        salida1 = true;
+                    }
+
+                    else if (opc == 2) //Si no, saldras del programa
+                    {
+                        Salir = true;
+                        salida1 = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR");
+                        Console.ReadKey();
+                    }
+
+                } while (salida1 == false);
+
+            } while (Salir == false); //Volveras a empezar 
+        }
+    }
+}
